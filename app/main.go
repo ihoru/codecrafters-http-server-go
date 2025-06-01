94cbbf27c27db7a1292e76bb09235b88e8f19e9d
Igor Polyakov <igor.polyakov@emergingtravel.com>
2025-06-01 08:40:49 +0400

    echo endpoint

--- app/main.go
+++ app/main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -64,23 +65,50 @@ func handleConnection(conn net.Conn) {
 		fmt.Println("Invalid HTTP request format")
 		return
 	}
-	fmt.Println("requestTarget:", requestTarget)
+	fmt.Println("Request:", requestTarget)
 
 	method := parts[0]
 	path := parts[1]
 	httpVersion := parts[2]
 
-	var response string
-	if method == "GET" && path == "/" && httpVersion == "HTTP/1.1" {
-		response = "HTTP/1.1 200 OK"
+	var statusLine string
+	var body string
+	headers := make(map[string]string)
+	if httpVersion != "HTTP/1.1" {
+		statusLine = "HTTP/1.1 426 Upgrade Required"
+		headers["Upgrade"] = "HTTP/1.1"
+	} else if method != "GET" {
+		statusLine = "HTTP/1.1 405 Not Allowed"
 	} else {
-		response = "HTTP/1.1 404 Not Found"
+		statusLine = "HTTP/1.1 200 OK"
+		if path == "/" {
+		} else if after, found := strings.CutPrefix(path, "/echo/"); found {
+			statusLine = "HTTP/1.1 200 OK"
+			body = after
+		} else {
+			statusLine = "HTTP/1.1 404 Not Found"
+		}
 	}
-	_, err := conn.Write([]byte(response + "\r\n\r\n"))
+	if body != "" {
+		if headers["Content-Type"] == "" {
+			headers["Content-Type"] = "text/plain"
+		}
+		headers["Content-Length"] = strconv.Itoa(len(body))
+	}
+	lines := make([]string, 0, 3+len(headers))
+	lines = append(lines, statusLine)
+	for k, v := range headers {
+		lines = append(lines, fmt.Sprintf("%s: %s", k, v))
+	}
+	lines = append(lines, "")
+	lines = append(lines, body)
+
+	response := strings.Join(lines, "\r\n")
+	_, err := conn.Write([]byte(response))
 	if err != nil {
 		fmt.Println("Error sending response:", err)
 		return
 	}
 
-	fmt.Println("Response sent successfully:", response)
+	fmt.Println("Response:", statusLine)
 }
